e56d27aca91348550a339a585f175166ce39dcfc
bryanyurialvesdelima <bryanyurialvesdelima@gmail.com>
2026-03-05 14:16:09 +0000

    Finalizar if-else e introdução aos Arrays

---
+++ arrays/arrays-time.js
@@ -0,0 +1,12 @@
+// Um array é um grupo indeixado de valores
+// Podem ser homogêneros / heterogêneros
+// Seus índices vão de "0"..."infinito"
+// Possuem funções nativas (prontas)
+// Podem ser Interados e Checados
+
+const time =['Petter', 'Pan', 'Julia']
+   // indices    0       1       3
+
+   console.log(time)
+   console.log(time[1])
+   console.log(time[32])
