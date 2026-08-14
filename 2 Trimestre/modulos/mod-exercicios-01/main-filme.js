8457a6766f724cb9b8e92b7a29d71dadef9ac508
bryanyurialvesdelima <bryanyurialvesdelima@gmail.com>
2026-08-14 11:13:26 +0000

    Implementação do servidor e criação das páginas de index e compras

---
+++ 2 Trimestre/modulos/mod-exercicios-01/main-filme.js
@@ -0,0 +1,7 @@
+import {alugarFilme} from './alugar-filme.js'
+
+console.log(alugarFilme('Meu malvado favorito 2', 'R$ 34,99'))
+
+console.log(alugarFilme('Carros 3', 'R$ 29,99'))
+
+console.log(alugarFilme('Como treinar seu Dragão 2', 'R$ 44,99'))
